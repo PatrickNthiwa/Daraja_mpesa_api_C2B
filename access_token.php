950e55ec20635779bf91a3f4e100d11046b522a7
patrick-Wambua <pwnthiwa@gmail.com>
2022-11-10 17:27:32 +0300

    added inde page

--- access_token.php
+++ access_token.php
@@ -2,7 +2,7 @@
 $consumerKey='x4lgMO695XnAGH9zclFxS6EDbMdSXr8G';
 $consumerSecret='JEXDLHu5pYeG3LxF';
 
-$headers=['Content-Type:application/json; charset-utf8'];
+$headers=['Content-Type:application/json;  charset-utf8'];
 
 $url='https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 
@@ -10,15 +10,18 @@ $curl= curl_init();
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 
-curl_setopt($curl, CURLOPT_HEADER, false);
-$credentials = base64_encode($consumer_key . ':' . $consumer_secret);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic ' . $credentials));
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+
+//$credentials = base64_encode($consumer_key . ':' . $consumer_secret);
+curl_setopt($curl, CURLOPT_USERPWD,$consumerKey .':'.$consumerSecret);
 
 $result=curl_exec($curl);
-$status= curl_getinfo($curl,CURLINFO_HTTP_CODE);
+$status= curl_getinfo($ch,CURLINFO_HTTP_CODE);
 $result=json_decode($result);
 
 $access_token=$result->access_token;
-echo '$access_token';
-curl_close($curl);
 
+echo $access_token;
+
+curl_close($curl);
+?>
