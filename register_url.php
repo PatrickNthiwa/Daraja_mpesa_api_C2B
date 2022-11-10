5ecc1e48acf7603404f194d2594203f4daa0bb14
patrick-Wambua <pwnthiwa@gmail.com>
2022-11-10 19:09:11 +0300

    removed access token code i the register file

--- register_url.php
+++ register_url.php
@@ -1,25 +1,4 @@
 <?php
-$consumerKey='x4lgMO695XnAGH9zclFxS6EDbMdSXr8G';
-$consumerSecret='JEXDLHu5pYeG3LxF';
-
-//$headers=['Content-Type:application/json; charset-utf8'];
-
-$url='https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-
-$curl = curl_init();
-curl_setopt($curl,CURLOPT_URL,$url);
-
-$credentials = base64_encode($consumer_key . ':' . $consumer_secret);
-curl_setopt($curl_Tranfer, CURLOPT_HTTPHEADER, array('Authorization: Basic ' . $credentials));
-
-curl_setopt($curl, CURLOPT_HEADER, true);
-//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-$curl_response = curl_exec($curl);
-
-$result= json_decode($curl_response);
-echo $result;
-
 $url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
 
 $curl  = curl_init();
