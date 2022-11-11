8571c47bc65b07cd58c01a83f7c84e4d28b4be16
patrick-Wambua <pwnthiwa@gmail.com>
2022-11-11 13:44:42 +0300

    Debugging using postman

--- simulate.php
+++ simulate.php
@@ -1,19 +1,19 @@
-<?php
+<?
 $ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate');
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Authorization: Bearer ZTEacRUK6NG1uEJd1L0y5vA8iHbL',
+    'Authorization: Bearer CEEqZpf2Qf4QamRieaIGv1BjI1eh',
     'Content-Type: application/json'
 ]);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, {
-    "ShortCode": 600996,
-    "CommandID": "CustomerPayBillOnline",
-    "amount": "1",
-    "MSISDN": "254705912645",
-    "BillRefNumber": "wambua",
-  });
+curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+    "ShortCode" => "600996",
+    "CommandID"=> "CustomerPayBillOnline",
+    "amount"=> "1",
+    "MSISDN"=> "254705912645",
+    "BillRefNumber"=> "wambua",
+));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $response     = curl_exec($ch);
 curl_close($ch);
+
 echo $response;
-?>
